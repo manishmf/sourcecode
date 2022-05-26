bfa2583a9cc79d4201466202a16c5aac928323e5
manishmf <manishmf.067@gmail.com>
2022-05-26 13:48:14 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,13 @@
 provider "aws" {
   region = var.region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 variable "region" {}
 variable "bucket_name" {}
+variable "access_key" {}
+variable "secret_key" {}
 
 resource "aws_s3_bucket" "example" {
   bucket = var.bucket_name
