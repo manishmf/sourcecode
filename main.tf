701d29db3b5094d555c43d08cce60d2a510c6020
manishmf <manishmf.067@gmail.com>
2022-05-27 16:13:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,6 +6,14 @@ provider "aws" {
   }
 }
 
+terraform {
+  backend "s3" {
+    bucket = "abcterrxyz"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 variable "region" {}
 variable "bucket_name" {}
 variable "role_name" {}
