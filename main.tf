8572c97cd2d769e5b0faf14dfc3c10c0ec868490
manishmf <manishmf.067@gmail.com>
2022-05-27 13:50:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,13 +1,14 @@
 provider "aws" {
   region = var.region
   assume_role {
-    role_arn     = "arn:aws:iam::483229333212:role/testrole"
+    role_arn     = var.role_name
     session_name = "my_account"
   }
 }
 
 variable "region" {}
 variable "bucket_name" {}
+variable "role_name" {}
 #variable "access_key" {}
 #variable "secret_key" {}
 
